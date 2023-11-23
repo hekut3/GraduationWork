ebb4688d2912845615ae96a014700d018a4acba0
peca2003 <pyotr.kostenkoyandex.ru@gmail.com>
2023-11-23 17:37:31 +0300

    Adding a background to menu

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -3,5 +3,6 @@ extends Node2D
 func _on_quit_pressed():
 	get_tree().quit()
 
+
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://map.tscn")
+	get_tree().change_scene_to_file("res://Map/map.tscn")
