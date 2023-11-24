b1d26a3951217a90ff82307651d062868739eeef
peca2003 <pyotr.kostenkoyandex.ru@gmail.com>
2023-11-24 21:28:48 +0300

    Adding buttons to the menu

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
+func _on_texture_button_pressed():
+	get_tree().change_scene_to_file("res://Map/map.tscn")
+
 func _on_quit_pressed():
 	get_tree().quit()
-
-
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Map/map.tscn")
