fd8bc30288f2d1723082cb036189c04322a7d6e2
peca2003 <pyotr.kostenkoyandex.ru@gmail.com>
2023-12-20 12:21:37 +0300

    Adding cutscene frames

--- Cutscene/Cutscene.gd
+++ Cutscene/Cutscene.gd
@@ -4,8 +4,3 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("Clouds")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
