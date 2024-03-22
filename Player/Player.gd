c55a9b308e347069d60cf09f845fdf48b9a8be25
peca2003 <pyotr.kostenkoyandex.ru@gmail.com>
2024-03-22 15:39:18 +0300

    Adding movement animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var speed = 70 # Скорость передвижения персонажа
-@onready var animation_player = $AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
 
 func _physics_process(_delta):
 	velocity = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down")).normalized() * speed
@@ -9,18 +9,13 @@ func _physics_process(_delta):
 	
 	# Анимация персонажа
 	var dir = velocity.normalized()
-	if (dir.x < 0 and dir.y < 0) or (dir.x > 0 and dir.y < 0):
-		animation_player.play("Walking_up")
-	elif (dir.x > 0 and dir.y > 0) or (dir.x < 0 and dir.y > 0):
-		animation_player.play("Walking_down")
-	elif dir == Vector2.UP:
-		animation_player.play("Walking_up")
-	elif dir == Vector2.DOWN:
-		animation_player.play("Walking_down")
+	if (dir.x < 0 and dir.y < 0) or (dir.x > 0 and dir.y < 0) or dir == Vector2.UP:
+		animation_player.play("Walk_up")
+	elif (dir.x > 0 and dir.y > 0) or (dir.x < 0 and dir.y > 0) or dir == Vector2.DOWN:
+		animation_player.play("Walk_down")
 	elif dir == Vector2.LEFT:
-		animation_player.play("Walking_to_left")
+		animation_player.play("Walk_left")
 	elif dir == Vector2.RIGHT:
-		animation_player.play("Walking_to_right")
+		animation_player.play("Walk_right")
 	else:
 		animation_player.play("Idle")
-	
