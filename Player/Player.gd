b3b406b7c9d0b5581bfda74a4c98d8e15bce1818
peca2003 <pyotr.kostenkoyandex.ru@gmail.com>
2024-03-23 17:14:34 +0300

    Update movement animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,16 +6,14 @@ var speed = 70 # Скорость передвижения персонажа
 func _physics_process(_delta):
 	velocity = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down")).normalized() * speed
 	move_and_slide()
-	
-	# Анимация персонажа
-	var dir = velocity.normalized()
-	if (dir.x < 0 and dir.y < 0) or (dir.x > 0 and dir.y < 0) or dir == Vector2.UP:
-		animation_player.play("Walk_up")
-	elif (dir.x > 0 and dir.y > 0) or (dir.x < 0 and dir.y > 0) or dir == Vector2.DOWN:
-		animation_player.play("Walk_down")
-	elif dir == Vector2.LEFT:
-		animation_player.play("Walk_left")
-	elif dir == Vector2.RIGHT:
-		animation_player.play("Walk_right")
+	update_animation()
+
+func update_animation():
+	if velocity.length() == 0:
+		animation_player.stop()
 	else:
-		animation_player.play("Idle")
+		if velocity.y < 0: animation_player.play("Walk_up")
+		elif velocity.x < 0: animation_player.play("Walk_left")
+		elif velocity.x > 0: animation_player.play("Walk_right")
+		else:
+			animation_player.play("Walk_down")
