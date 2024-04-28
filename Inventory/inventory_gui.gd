a4319649aee24f0afaac594ff786e21a70a11f23
peca2003 <pyotr.kostenkoyandex.ru@gmail.com>
2024-04-28 16:13:33 +0300

    Adding new sprites

--- Inventory/inventory_gui.gd
+++ Inventory/inventory_gui.gd
@@ -5,6 +5,17 @@ signal closed
 
 var is_open_inventory: bool = false
 
+@onready var inventory: Inventory = preload("res://Inventory/playerInventory.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+
+func _ready():
+	inventory.updated.connect(update)
+	update()
+
+func update():
+	for i in range(min(inventory.slots.size(), slots.size())):
+		slots[i].update(inventory.slots[i])
+
 func open():
 	visible = true
 	is_open_inventory = true
--- Player/Player.gd
+++ Player/Player.gd
@@ -2,42 +2,33 @@ extends CharacterBody2D
 
 signal  health_changed
 
-var speed = 70 # Скорость передвижения персонажа
+var speed = 60 # Скорость передвижения персонажа
 @onready var animation_player = $AnimationPlayer
 @onready var hurt_timer = $HurtTimer
 @onready var hurt_box = $HurtBox
+@onready var effects = $Effects
+
 @export var max_health = 3
 @onready var current_health: int = max_health
 
-@export var knock_back_power: int = 300
+@export var knockbackPower: int = 400
 
-var is_hurt: bool = false
+@export var inventory: Inventory
 
-func _physics_process(_delta):
-	velocity = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down")).normalized() * speed
-	move_and_slide()
-	update_animation()
-	if !is_hurt:
-		for area in hurt_box.get_overlapping_areas():
-			if area.name == "HitBox":
-				hurt_by_enemy(area)
+var isHurt: bool = false
+var lastAnimDirection: String = "Down"
 
-func hurt_by_enemy(area):
-	current_health -= 1
-	if current_health < 0:
-		current_health = max_health
-	#print(current_health)
-	print(area.get_parent().name)
-	health_changed.emit(current_health)
-	is_hurt = true
-	knock_back(area.get_parent().velocity)
-	hurt_timer.start()
-	await hurt_timer.timeout
-	is_hurt = false
+func _ready():
+	effects.play("RESET")
 
-func update_animation():
+func handleInput():
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection * speed
+
+func updateAnimation():
 	if velocity.length() == 0:
-		animation_player.stop()
+		if animation_player.is_playing():
+			animation_player.stop()
 	else:
 		if velocity.y < 0: animation_player.play("Walk_up")
 		elif velocity.x < 0: animation_player.play("Walk_left")
@@ -45,21 +36,43 @@ func update_animation():
 		else:
 			animation_player.play("Walk_down")
 
+func _physics_process(_delta):
+	handleInput()
+	move_and_slide()
+	updateAnimation()
+	if !isHurt:
+		for area in hurt_box.get_overlapping_areas():
+			if area.name == "HitBox":
+				hurtByEnemy(area)
+
+func hurtByEnemy(area):
+	current_health -= 1
+	if current_health < 0:
+		current_health = max_health
+		
+	health_changed.emit(current_health)
+	isHurt = true
+	knockback(area.get_parent().velocity)
+	effects.play("hurtBlink")
+	hurt_timer.start()
+	await hurt_timer.timeout
+	effects.play("RESET")
+	isHurt = false
+
 func _on_hurt_box_area_entered(area):
 	if area.has_method("collect"):
-		area.collect()
+		area.collect(inventory)
 
-func knock_back(enemy_velocity: Vector2):
-	var knock_back_direction = (enemy_velocity - velocity).normalized() * knock_back_power
-	velocity = knock_back_direction
+func knockback(enemyVelocity: Vector2):
+	var knockbackDirection = (enemyVelocity - velocity).normalized() * knockbackPower
+	velocity = knockbackDirection
 	move_and_slide()
 
 func _on_inventory_gui_closed():
 	get_tree().paused = false
 
-
 func _on_inventory_gui_opened():
 	get_tree().paused = true
 
-
-func _on_hurt_box_area_exited(area): pass
+func _on_hurt_box_area_exited(area):
+	pass
