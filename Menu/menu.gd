9ea313e4bf7d31c9830344175a8526167d028fbc
peca2003 <pyotr.kostenkoyandex.ru@gmail.com>
2024-06-12 18:51:36 +0300

    Fixing the buttons

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
 func _on_texture_button_pressed():
-	get_tree().change_scene_to_file("res://Map/map.tscn")
+	get_tree().change_scene_to_file("res://Cutscene/Cutscene.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
 
-
-func _on_new_game_pressed():
-	get_tree().change_scene_to_file("res://Cutscene/Cutscene.tscn")
